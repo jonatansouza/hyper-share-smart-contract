0221ea44dfc15112c437c3cbaefc53af434d3b73
jonatan gall <jonatangd.souza@gmail.com>
2021-06-15 19:52:40 -0300

    interfaces and enums

--- src/shared-data.ts
+++ src/shared-data.ts
@@ -4,10 +4,44 @@
 
 import { Object, Property } from 'fabric-contract-api';
 
+export enum PermissionTypes {
+    Granted,
+    Denied,
+    NA
+}
+
+export interface SharedDataModel {
+    ownerId: string;
+    sharedWith: string[];
+    sharedDataDescription: string;
+    updated?: number;
+    requester?: string;
+    permission?: PermissionTypes;
+    mode?: string; // method name
+
+}
+
 @Object()
 export class SharedData {
 
     @Property()
-    public value: string;
+    public ownerId: string;
 
+    @Property()
+    public sharedWith: string[];
+
+    @Property()
+    public sharedDataDescription: string;
+
+    @Property()
+    public updated: number;
+
+    @Property()
+    public requester: string;
+
+    @Property()
+    public permission: PermissionTypes;
+
+    @Property()
+    public mode: string; // method name
 }
