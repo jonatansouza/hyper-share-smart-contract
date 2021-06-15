bbef5b7b183f2c67a43f523bdf097c45cbf3f1b1
jonatan gall <jonatangd.souza@gmail.com>
2021-06-15 19:53:07 -0300

    create and read methods

--- src/shared-data-contract.ts
+++ src/shared-data-contract.ts
@@ -3,7 +3,7 @@
  */
 
 import { Context, Contract, Info, Returns, Transaction } from 'fabric-contract-api';
-import { SharedData } from './shared-data';
+import { PermissionTypes, SharedData, SharedDataModel } from './shared-data';
 
 @Info({title: 'SharedDataContract', description: 'My Smart Contract' })
 export class SharedDataContract extends Contract {
@@ -16,48 +16,57 @@ export class SharedDataContract extends Contract {
     }
 
     @Transaction()
-    public async createSharedData(ctx: Context, sharedDataId: string, value: string): Promise<void> {
+    public async createSharedData(ctx: Context, sharedDataId: string, requester: string, value: SharedDataModel): Promise<void> {
         const exists: boolean = await this.sharedDataExists(ctx, sharedDataId);
         if (exists) {
             throw new Error(`The shared data ${sharedDataId} already exists`);
         }
         const sharedData: SharedData = new SharedData();
-        sharedData.value = value;
+        sharedData.ownerId = requester;
+        sharedData.sharedWith = [];
+        sharedData.sharedDataDescription = value.sharedDataDescription;
+        sharedData.mode = 'createSharedData';
+        sharedData.updated = new Date().getTime();
+        sharedData.requester = requester;
+        sharedData.permission = PermissionTypes.NA;
         const buffer: Buffer = Buffer.from(JSON.stringify(sharedData));
         await ctx.stub.putState(sharedDataId, buffer);
     }
 
     @Transaction(false)
     @Returns('SharedData')
-    public async readSharedData(ctx: Context, sharedDataId: string): Promise<SharedData> {
+    public async readSharedData(ctx: Context, requester: string, sharedDataId: string): Promise<SharedData> {
         const exists: boolean = await this.sharedDataExists(ctx, sharedDataId);
         if (!exists) {
             throw new Error(`The shared data ${sharedDataId} does not exist`);
         }
         const data: Uint8Array = await ctx.stub.getState(sharedDataId);
         const sharedData: SharedData = JSON.parse(data.toString()) as SharedData;
+        if(sharedData.ownerId !== requester) {
+            throw new Error(`The shared data ${sharedDataId} does not belong to ${requester}`);
+        }
         return sharedData;
     }
 
-    @Transaction()
-    public async updateSharedData(ctx: Context, sharedDataId: string, newValue: string): Promise<void> {
-        const exists: boolean = await this.sharedDataExists(ctx, sharedDataId);
-        if (!exists) {
-            throw new Error(`The shared data ${sharedDataId} does not exist`);
-        }
-        const sharedData: SharedData = new SharedData();
-        sharedData.value = newValue;
-        const buffer: Buffer = Buffer.from(JSON.stringify(sharedData));
-        await ctx.stub.putState(sharedDataId, buffer);
-    }
+    // @Transaction()
+    // public async updateSharedData(ctx: Context, sharedDataId: string, newValue: string): Promise<void> {
+    //     const exists: boolean = await this.sharedDataExists(ctx, sharedDataId);
+    //     if (!exists) {
+    //         throw new Error(`The shared data ${sharedDataId} does not exist`);
+    //     }
+    //     const sharedData: SharedData = new SharedData();
+    //     sharedData.value = newValue;
+    //     const buffer: Buffer = Buffer.from(JSON.stringify(sharedData));
+    //     await ctx.stub.putState(sharedDataId, buffer);
+    // }
 
-    @Transaction()
-    public async deleteSharedData(ctx: Context, sharedDataId: string): Promise<void> {
-        const exists: boolean = await this.sharedDataExists(ctx, sharedDataId);
-        if (!exists) {
-            throw new Error(`The shared data ${sharedDataId} does not exist`);
-        }
-        await ctx.stub.deleteState(sharedDataId);
-    }
+    // @Transaction()
+    // public async deleteSharedData(ctx: Context, sharedDataId: string): Promise<void> {
+    //     const exists: boolean = await this.sharedDataExists(ctx, sharedDataId);
+    //     if (!exists) {
+    //         throw new Error(`The shared data ${sharedDataId} does not exist`);
+    //     }
+    //     await ctx.stub.deleteState(sharedDataId);
+    // }
 
 }
